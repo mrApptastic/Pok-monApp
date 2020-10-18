87fae666ee5f9bb50d1d51566d179c8a89cf0fac
mrApptastic <henrikbeske@gmail.com>
2020-10-18 20:12:28 +0200

    Test stuff.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
+export default function Uha() {
   return (
+    <div>Hej </div>
+  );
+}
+
+export default function App() {
+  return (    
     <div>
+    <Uha/>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
+
