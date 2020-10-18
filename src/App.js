de48b0d74d0e4a72d93bf22779d559ccbd1f18b1
mrApptastic <henrikbeske@gmail.com>
2020-10-18 23:08:51 +0200

    More react stuff -yeah!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./style.css";
 
-export default function Uha() {
+const element = <h1>Hello, world!</h1>;
+
+function Uha() {
   return (
-    <div>Hej </div>
+    <ul>
+      <li>
+        Hej hej
+      </li>
+    </ul>
   );
 }
 
 export default function App() {
   return (    
     <div>
-    <Uha/>
+    {element}
+    <Uha />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
