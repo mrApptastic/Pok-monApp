143603dbee08e761fb4e4354e53aae3b9f71cba0
mrApptastic <henrikbeske@gmail.com>
2020-10-19 11:31:54 +0200

    Added basic list component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./style.css";
 
 const element = <h1>Hello, world!</h1>;
+
+function getPokeUrl(url) {
+  let number = url.replace('https://pokeapi.co/api/v2/pokemon/','').replace('/','');
+
+  while (number.length < 3) {
+    number = "0" + number;
+  }
+
+  return "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + number + ".png";
+}
+
+function MyComponent() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=20")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.results);
+          setCount(result.count);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <div>{count}</div>
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              <span>{item.name}</span> 
+              <img src={getPokeUrl(item.url)} alt={item.name} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 function Uha() {
   return (
@@ -17,6 +70,7 @@ export default function App() {
   return (    
     <div>
     {element}
+    <MyComponent />
     <Uha />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
